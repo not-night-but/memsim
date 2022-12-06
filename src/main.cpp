e0eb77ce68e96d5cab46a1f9b4d07627f40feecf
Day Matchullis <dsm6069@gmail.com>
2022-12-06 13:09:43 -0700

    Simulation working, implemented ncurses frontend

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <algorithm>
 #include <chrono>
 #include <deque>
-#include <iostream>
 #include <iterator>
+#include <curses.h>
+#include <string>
+#include <sstream>
+#include <random>
+#include <iostream>
 
 class Process {
    public:
     // Constructor ----------------------
     Process() {
         m_duration = rand() % 10 + 1;
-        m_size = rand() % 250 + 1;
+        m_size = (rand() % 25 + 1) * 10;
         m_jobNo = s_lastJobNo += 1;
     }
 
@@ -49,6 +53,8 @@ class MemMegaByte {
     void deallocate() { m_allocatedToJob = -1; }
 
     bool isAllocated() { return m_allocatedToJob != -1; }
+
+    const int getAllocatedToJob() const { return m_allocatedToJob; }
 
    private:
     // The job number this block is allocated to, -1 if not allocated.
@@ -92,22 +98,113 @@ int GetStartIndexOfNextFitBlock(int currentIndex, Process proc) {
 }
 
 void AllocateMemory(int startIndex, Process proc) {
-    std::cout << "Allocating " << proc.getSize() << "mb of memory" << std::endl;
+    // std::cout << "Allocating " << proc.getSize() << "mb of memory" << std::endl;
     for (int i = startIndex; i < startIndex + proc.getSize(); i++) {
         mem[i].allocateTo(proc.getJobNo());
     }
 }
 
 void DeallocateMemory(Process proc) {
-    std::cout << "Deallocating " << proc.getSize() << "mb of memory"
-              << std::endl;
+    // std::cout << "Deallocating " << proc.getSize() << "mb of memory"
+    //           << std::endl;
     for (int i = proc.getStartIndex();
          i < proc.getStartIndex() + proc.getSize(); i++) {
         mem[i].deallocate();
     }
 }
 
+std::string streamToString(std::stringstream& ss) {
+  std::string str = "", temp = "";
+  while (!ss.eof()) {
+    ss >> temp;
+    str += temp + " ";
+  }
+  str = str.substr(0, str.size() - 1);
+  ss.str(std::string());
+  ss.clear();
+  return str;
+}
+
+WINDOW *leftWin, *rightWin;
+int terminalX, terminalY;
+
+void initNCurses() {
+    setlocale(LC_ALL, "");
+    initscr(); 
+    start_color();
+
+    getmaxyx(stdscr, terminalY, terminalX);
+    
+    leftWin = newwin(terminalY, terminalX * 0.25, 0, 0);
+    rightWin = newwin(terminalY, terminalX * 0.75, 0, (terminalX * 0.25) + 1);
+    box(leftWin, 0, 0);
+    box(rightWin, 0, 0);
+}
+
+void displayProcess(Process process, std::string preText, int y, int x) {
+    mvwprintw(leftWin, y, x, "%s%*i      %*iu      %imb", preText.c_str(), 2, process.getJobNo(), 2, process.getDuration(), process.getSize());
+}
+
+void initColors() {
+    // Empty Block
+    init_pair(1, COLOR_WHITE, COLOR_WHITE);
+
+    // Filled blocks
+    init_pair(2, COLOR_BLACK, COLOR_RED);
+    init_pair(3, COLOR_BLACK, COLOR_GREEN);
+    init_pair(4, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(5, COLOR_BLACK, COLOR_BLUE);
+    init_pair(6, COLOR_BLACK, COLOR_MAGENTA);
+    init_pair(7, COLOR_BLACK, COLOR_CYAN);
+}
+
+void drawBlocks() {
+    std::stringstream ss;
+    int i = 0;
+    int nextColor = 1;
+    int lastId = -1;
+    for (int x = 2; x <= 30; x = x + 3) {
+        for (int y = 1; y < 11; y++) {
+            auto block = mem[i * 10];
+            int pair = 1;
+            char firstChar = ' ', secondChar = ' ';
+            if (block.isAllocated()) {
+                std::string str;
+                int id = block.getAllocatedToJob();
+                if (id != lastId) {
+                    nextColor += 1;
+                    if (nextColor > 7) nextColor = 2;
+                    lastId = id;
+                }
+                pair = nextColor;
+                ss << block.getAllocatedToJob();
+                ss >> str;
+                ss.clear();
+                if (str.length() == 2) {
+                    firstChar = (char)str[0];
+                    secondChar = (char)str[1];
+                } else {
+                    firstChar = '0';
+                    secondChar = (char)str[0];
+                }
+            }
+            mvwaddch(rightWin, y, x, firstChar | COLOR_PAIR(pair));
+            mvwaddch(rightWin, y, x + 1, secondChar | COLOR_PAIR(pair));
+            wrefresh(rightWin);
+            i++;
+        }
+    }
+}
+
+
 int main() {
+    clear();
+    refresh();
+    initNCurses();
+    initColors();
+    wrefresh(leftWin);
+    wrefresh(rightWin);
+
     bool running = true;
     // The processes that are "Running"
     std::deque<Process> processes = std::deque<Process>();
@@ -119,27 +216,37 @@ int main() {
         processes.push_front(Process());
     }
 
-    std::random_shuffle(processes.begin(), processes.end(),
-                        [&](int i) { return std::rand() % i; });
+    std::mt19937 rng(1234);
+    std::shuffle(processes.begin(), processes.end(), rng);
 
     int currentMemindex = 0;
     // "Run" the processes
+    drawBlocks();
+
     while (running) {
+        wclear(leftWin);
+        box(leftWin, 0, 0);
         auto start = std::chrono::steady_clock::now();
         // Get the next process
         auto process = processes.front();
         bool processIsActive = false;
-
-        std::cout << process.getJobNo() << "  " << process.getDuration()
-                  << "cycles"
-                  << "  " << process.getSize() << "mb" << std::endl;
+        
+        mvwprintw(leftWin, 1, 1, "        JobNo   Time     Size");
+        if (!processes.empty())
+            displayProcess(process, "Current ", 2, 1);  
+        else 
+            mvwprintw(leftWin, 2, 1, "Current");
+        mvwprintw(leftWin, 3, 1, "------------------------------");
+        mvwprintw(leftWin, terminalY - 2, 1, "Jobs in Queue: %li", processes.size());
 
         // Run Next fit algo
-        int index = GetStartIndexOfNextFitBlock(currentMemindex, process);
-        if (index != -1) {
-            AllocateMemory(index, process);
-            process.setStartIndex(index);
-            processIsActive = true;
+        if (!processes.empty()) {
+            int index = GetStartIndexOfNextFitBlock(currentMemindex, process);
+            if (index != -1) {
+                AllocateMemory(index, process);
+                process.setStartIndex(index);
+                processIsActive = true;
+            }
         }
 
         int i = 0;
@@ -152,11 +259,15 @@ int main() {
                 DeallocateMemory(*p);
                 active.erase(iter);
             } else {
+                displayProcess(*p, "        ", i + 4, 1);
                 (*p).decrementDuration();
                 i++;
             }
         }
-        // replacement algo?
+        wclear(rightWin);
+        box(rightWin, 0, 0);
+        drawBlocks();
+        wrefresh(leftWin);
 
         // delay end of loop.
         auto end = std::chrono::steady_clock::now();
@@ -167,8 +278,11 @@ int main() {
             processes.pop_front();
             active.push_back(process);
         }
-        if (processes.empty()) running = false;
+        if (processes.empty() && active.empty()) running = false;
     }
+    endwin();
+    delwin(leftWin);
+    delwin(rightWin);
     std::cin.get();
     return 1;
 }
